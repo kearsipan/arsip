75cd2ad854196ebf7dfe22f0075355357d7d8ee2
nadia\nadia_atiq <nadiaatiqur123@gmail.com>
2018-01-26 17:00:06 +0700

    tambah tabel desposisi

--- application/views/super-admin/detail.php
+++ application/views/super-admin/detail.php
@@ -11,7 +11,7 @@
     <?php include 'navigasi.php'; ?>
 
                 <form method="post" action="<?php echo base_url('index.php/Welcome/add_despos'); ?>">
-                    <div class="col-lg-6 col-md-6">
+                    <div class="col-lg-12 col-md-12">
                         <div class="card">
                             <div class="header">
                                 <h4 class="title">Tambahkan desposisi</h4>
@@ -19,78 +19,60 @@
                             <div class="content">
                                 <form>
                                     <div class="row">
-                                        <div class="col-md-12">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Tanggal Surat</label>
+                                                <input type="date" class="form-control border-input" name="tanggal">
+                                            </div>
                                             <div class="form-group">
                                                 <label>Asal Surat</label>
                                                 <input type="text" class="form-control border-input" name="asal">
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>No Surat</label>
+                                                <input type="text" class="form-control border-input" name="nosur">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Perihal</label>
+                                                <input type="text" class="form-control border-input" name="perihal">
+                                            </div>
                                             <div class="form-group">
                                                 <label>Tanggal Diterima</label>
                                                 <input type="date" class="form-control border-input" name="diterima">
                                             </div>
+                                            <div class="form-group">
+                                                <label>Kode Agenda</label>
+                                                <input type="text" class="form-control border-input" name="kode">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
+                                        <div class="col-md-6">
                                             <div class="form-group">
                                                 <label>Pemberi Desposisi</label>
                                                 <input type="text" class="form-control border-input" name="pemberi">
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Terusan</label>
-                                                <input type="text" class="form-control border-input" name="terusan">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
                                             <div class="form-group">
                                                 <label>Untuk</label>
                                                 <input type="text" class="form-control border-input" name="untuk">
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
                                             <div class="form-group">
                                                 <label>Isi Desposisi</label>
                                                 <input type="textarea" class="form-control border-input" name="isi">
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                        <div class="form-group">
+                                            <div class="form-group">
                                             <label>Diteruskan ke: </label>
-                                    <div class="dropdown">
-                                          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                            Terusan
-                                            <span class="caret"></span>
-                                          </button>
-                                          <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                                            <li><a href="#">KTU</a></li>
-                                            <li><a href="#">WK1</a></li>
-                                            <li><a href="#">WK2</a></li>
-                                            <li><a href="#">WK3</a></li>
-                                            <li><a href="#">WK4</a></li>
-                                            <li><a href="#">KUP</a></li>
-                                            <li><a href="#">Lainnya</a></li>
-                                            
-                                          </ul>
-                                    </div>
-                                        </div>
-                                    </div>
-                                    </div>  
-                                    <div class="text-center">
-                                        <button type="submit" class="btn btn-info btn-fill btn-wd">Tambah</button>
+                                            <select name="terusan" class="form-control">
+                                                <option value="KTU">KTU</option>
+                                                <option value="WK1">WK1</option>
+                                                <option value="WK2">WK2</option>
+                                                <option value="WK3">WK3</option>
+                                                <option value="KUP">KUP</option>
+                                                <option value="Lainnya">Lainnya</option>
+                                            </select>
+                                            </div>
+                                            <div class="form-control">
+                                                <button type="submit" class="btn btn-info btn-fill btn-wd">Tambah</button>
+                                            </div>
+                                        </div>  
                                     </div>
                                     <div class="clearfix"></div>
                                 </form>
